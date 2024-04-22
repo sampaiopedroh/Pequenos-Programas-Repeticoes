f22e73c707aba4540f4579bd48268deec13ef495
Pedro Sampaio <98555328+sampaiopedroh@users.noreply.github.com>
2024-04-22 09:08:44 -0300

    Adicionando os primeiros programas com for

---
+++ Pequenos Programas repetições como while/For/For.py
@@ -0,0 +1,46 @@
+# for char in 'pedro':
+#     print(char)
+
+# for i in range(1, 10, 1): #start, end, step
+#     print(i)
+
+# Soma de 1 à 100
+# aux = 0
+# for i in range(1, 101):
+#     aux += i
+# print(aux)
+
+# 3 tentativas de senha
+# user = input('Diga seu usuário: ')
+# senha_certa = '1234'
+# senha = input('Diga sua senha: ')
+# for i in range(1, 2):
+#     if senha == senha_certa:
+#         break
+#     senha = input(f'Senha incorreta, mais {3 - i} tentativas: ')
+# print(f'Bem vindo {user}')
+
+# Soma de 10 n° aleatórios
+# num = 0
+# for i in range(1, 11):
+#     num += int(input(f'Diga o {i}° número: '))
+# print(f'A média da soma dos 10 números é: {num / 10}')
+
+# Todas as tabuádas
+# for tab in range(1, 11):
+#     for num in range(1, 11):
+#         print(f'{num} x {tab} = {num * tab}')
+
+# Fibo
+tam = int(input('Diga o tamanho da sequência: '))
+a = 0
+b = 0
+for i in range(1, (tam + 1)):
+    c = a + b
+    if c == 0:
+        print(1)
+        a += 1
+    else:
+        print(c)
+        b = a
+        a = c
