3dca842bdcea44ceb3e486ee7e37fd1fc1ced95b
Pedro Sampaio <98555328+sampaiopedroh@users.noreply.github.com>
2024-04-23 16:58:59 -0300

    Update Ex_2.py

--- Pequenos Programas repetições como while/Lista_loop/Ex_2.py
+++ Pequenos Programas repetições como while/Lista_loop/Ex_2.py
@@ -7,33 +7,44 @@
 
 nome = input('Diga seu nome: ')
 qnt_letras = len(nome)
-while qnt_letras <= 2:
+while qnt_letras < 3:
     nome = input('Diga um nome válido: ')
     qnt_letras = len(nome)
 
-idade = int(input('Diga sua idade: '))
-while not 0 < idade <= 150:
-    idade = int(input('Diga uma idade válida: '))
+idade = input('Diga sua idade: ')
+while not idade.isnumeric() or int(idade) > 150:
+    idade = input('Diga uma idade válida: ')
 
-sal = int(input('Diga seu salário: '))
-while sal <= 0:
-    sal = int(input('Diga um salário válido: '))
+sal = input('Diga seu salário (sem os centavos): ')
+while not sal.isnumeric() or int(sal) < 0:
+    sal = input('Diga um salário válido: ')
 
-sexo = input('Diga seu sexo ("f" ou "m"): ')
-while True:
-    if sexo == 'f' or sexo == 'm':
-        break
-    sexo = input('Digite um sexo válido: ')
+sexo = input('Diga seu sexo:\n'
+             'm - masculino\n'
+             'f - feminino')
+while sexo != 'm' or sexo != 'm':
+    sexo = input('Diga um sexo válido')
 
-est_civil = input('Diga seu estado civil ("s", "c", "v" ou "d"): ')
-while True:
-    if est_civil == 's' or est_civil == 'c' or est_civil == 'v' or est_civil == 'd':
-        break
+est_civil = input('Informe seu estado civil:\n'
+                  's - solteiro(a)\n'
+                  'c - casado(a)\n'
+                  'v - viúvo(a)\n'
+                  'd - divorciado(a)')
+est_possiveis = ['s', 'c', 'v', 'd']
+while est_civil != est_possiveis:
     est_civil = input('Diga um estado civil válido: ')
+if est_civil == 's':
+    est_civil = 'Solteiro(a)'
+elif est_civil == 'c':
+    est_civil = 'Casado(a)'
+elif est_civil == 'v':
+    est_civil = 'Viúvo(a)'
+else:
+    est_civil = 'Divorsiado(a)'
 
 print(f'Seu nome: {nome}\n'
-      f'Sua idade: {idade}\n'
-      f'Seu salário: {sal}\n'
+      f'Sua idade: {idade} anos\n'
+      f'Seu salário: R${sal}\n'
       f'Seu sexo: {sexo}\n'
       f'Seu estado civil: {est_civil}')
 
