1b90c750001f7fa4f2224eab84d6d5dac7d6f0fd
Pedro Sampaio <98555328+sampaiopedroh@users.noreply.github.com>
2024-04-23 16:58:07 -0300

    Update Ex_1.py

--- Pequenos Programas repetições como while/Lista_loop/Ex_1.py
+++ Pequenos Programas repetições como while/Lista_loop/Ex_1.py
@@ -1,12 +1,25 @@
-# Faça um programa que peça uma nota, entre zero e dez.
-# Mostre uma mensagem caso o valor seja inválido e
-# continue pedindo até que o usuário informe um valor válido.
+# Quais dos n primeiros números da sequência de fibo são primos:
+qtd = int(input('Diga o tamanho da sequência a ser testada: '))
+a = 1
+b = 1
+j = 0
+k=2
+print(a)
+print(b)
+while j < qtd:
+    c = a + b
 
-# Normal:
-nota = int(input('Diga sua nota: '))
-while not (0 < nota <= 10):
-    nota = int(input('Diga uma nota válida: '))
-print(f'Sua nota é: {nota}')
+    a = b
+    b = c
+    i = 2
+    num = c
+    k += 1
+    while True:
 
-# Com .isnumeric()
-
+        if num % i == 0:
+            break
+        elif i >= num ** 0.5:
+            print(f"{num} É primo: {j + 1}º primo e {k}º fibo")
+            j += 1
+            break
+        i += 1
